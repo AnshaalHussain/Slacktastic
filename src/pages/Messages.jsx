7b7a8a1911dfaf5a701e559da76e19c2f0ddebfe
Anshaal Hussain <anshaalhussain@gmail.com>
2023-08-15 03:38:05 -0400

    create messages retrieval and UI

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import Layout from "./Layout";
 import SignOut from "../components/SignOut";
 import { Container, Stack } from "@mui/system";
+import Chat from "../components/Messages/Chat";
 
 const Messages = () => {
   return (
     <>
       <Layout>
         <Container maxWidth="sm">
-          <Stack>
-            Message Page
-            <SignOut />
-          </Stack>
+          <SignOut />
+          {/* <Stack> */}
+          <Chat />
+          {/* </Stack> */}
         </Container>
       </Layout>
     </>
